68268b20285d489c86f25a58ffc206dc1c40b824
Rikk Hill <rikk.hill@net-a-porter.com>
2015-02-25 14:55:48 +0000

    13 and 14

---
+++ euler_14.rb
@@ -0,0 +1,31 @@
+# Project Euler Problem 14
+
+def collatz_count(n)
+  n_prime = n
+  count = 0
+  while n_prime != 1
+    if n_prime % 2 == 0
+      n_prime = n_prime / 2
+    else
+      n_prime = (3 * n_prime) + 1
+    end
+    count +=1
+  end
+  return count
+end
+
+highest_count = 0
+highest_x = 0
+(1..999999).each do |x|
+  collatz = collatz_count(x)
+  if x % 100000 == 0
+    puts "#{x}: #{collatz}"
+  end
+  if collatz > highest_count
+    puts "#{x}: #{collatz}"
+    highest_count = collatz
+    highest_x = x
+  end
+end
+
+puts highest_x
