4db952495891dc87c40767cee0d5562b35a884cb
Rikk Hill <rikk.hill@net-a-porter.com>
2015-02-25 17:44:13 +0000

    15

--- euler_15.rb
+++ euler_15.rb
@@ -1,7 +1,11 @@
 # Project Euler Problem 15
 
-def gridpaths n, m
+# This is just 40C20
 
+def choose n, k
+  return (1..n).reduce(:*) / ( (1..k).reduce(:*) * (1..n-k).reduce(:*) )
 end
 
-gridpaths(2,2)
+## (It seems like `inject` and `reduce` are the same thing)
+
+puts choose(40, 20)
