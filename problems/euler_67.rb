08cf8889afc6e83ceafc09360afe630ee6fdc28e
Rikk Hill <rikk.hill@net-a-porter.com>
2015-03-02 14:38:37 +0000

    Nicer triangular reduction

--- problems/euler_67.rb
+++ problems/euler_67.rb
@@ -1,32 +1,13 @@
 # Project Euler Problem 67
-# Flagrantly copied from Problem 18
+# Flagrantly stolen fro Problem 18
 
 require_relative '../lib/euler_helpers.rb'
-require 'pp'
 include FileHelper
 
-def longest_triangle_path(triangle)
-
-  triangle.reverse!
-  merger = Array.new(triangle[0].count, 0)
-
-  triangle.each do |line|
-
-    merger = merger.zip(line).map{ |tup| tup[0] + tup[1] }
-    if merger.count == 1
-      return merger[0]
-    end
-
-    next_merger = []
-    (0..line.count - 2).each do |i|
-      next_merger << [ merger[i], merger[i+1] ].max
-    end
-    merger = next_merger
-
-  end
-  return merger
+def triangle_reductor(a,b)
+  return a.each_cons(2).map{|x| x.max}.zip(b).map{|a| a[0] + a[1]}
 end
 
 triangle = FileHelper.read_into_array('data/problem_67.txt')
 
-puts longest_triangle_path(triangle)
+puts triangle.reverse.reduce{ |x, y| triangle_reductor(x, y) }
